3a6bf5391d957535918b5fd2093d3dc6b20e5bac
mariaeloi <mariaeduardaeloi@outlook.com>
2022-06-26 16:51:01 -0300

    feat: gera código e adiciona campo num_users para atração

--- src/services/AttractionService.js
+++ src/services/AttractionService.js
@@ -6,13 +6,20 @@ class AttractionService {
     async save(attraction) {
         let errors = new Object();
 
-        const attractionByCode = await AttractionRepository.findByCode(attraction.code);
-        if(attractionByCode) {
-            errors.code = msg.attraction.codeExists;
-        }
+        attraction.code = `#${attraction.name.toUpperCase().replaceAll(' ', '_')}`;
+        attraction.num_users = parseInt(attraction.num_users);
+
         const attractionByName = await AttractionRepository.findByName(attraction.name);
         if(attractionByName) {
             errors.name = msg.attraction.nameExists;
+        }
+
+        if(attraction.duration <= 0) {
+            errors.duration = "A duração deve ser maior que 0";
+        }
+
+        if(attraction.num_users <= 0) {
+            errors.num_users = "A quantidade de vagas deve ser maior que 0";
         }
 
         if(Object.keys(errors).length !== 0) {
